76e140cf02bee4a4276cce092205d984848068f3
Dale <dellryuzi@gmail.com>
2026-05-21 09:32:08 +0700

    fix: decryption on new aes-key encryption system and collection refactorisation

--- scripts/fhe/config.js
+++ scripts/fhe/config.js
@@ -31,7 +31,9 @@ export const fheNftConfig = {
       // sepolia: "https://ethereum-sepolia-rpc.publicnode.com",
       // sepolia: "https://rpc.sepolia.ethpandaops.io",
     },
-    privateKey: process.env.PRIVATE_KEY_ETH || process.env.PRIVATE_KEY_ETH_2,
+    privateKey:
+      "a9d27031d50729345639276dbc8e0708215fc888955272ea0d9fae78d5223bf8",
+    // privateKey: process.env.PRIVATE_KEY_ETH || process.env.PRIVATE_KEY_ETH_2,
     privateKey2: process.env.PRIVATE_KEY_ETH_2,
     backendPrivateKey:
       process.env.BACKEND_PRIVATE_KEY || process.env.PRIVATE_KEY_ETH,
@@ -42,18 +44,20 @@ export const fheNftConfig = {
   },
   erc721mg: {
     collectionAddress: "0xFBf8608D465D4Aa88b7fDb4Bb76c84cb7037AE55",
+    latestCollectionAddress: "0x4cf031C2ecf8ee6b08bF7ab16a49636A0FADBF9D",
+    gatewayAddress: "0xA91D70aE85af28Efc23D5d90348a72A08C56056A",
     mint: {
-      to: "0xA31A54e4C258B1BE8cE887a2724906BfCe88Cc6A",
+      to: "0x72776B37a55d502E81C29103b89e84EcC81BD63d",
+      // to: "0xA31A54e4C258B1BE8cE887a2724906BfCe88Cc6A",
       uri: "https://metadata.mogate.xyz/erc721mg/test.json",
-      plaintextGiftcode: "MOGATE_TEST_GIFTCODE_001",
-      // AES key is generated randomly per mint
-      cipherRef: "",
+      plaintextGiftcode: "other-acc-new-est-test-new-giftcode-uuid-123",
     },
     decrypt: {
-      tokenId: 3,
-      cipherRef: "giftcode_1776618540095.bin",
-      giftcode: "MOGATE_TEST_GIFTCODE_540094", // For testing only
-      aesKeyHex: "0x16c906e990ef08c2ac500f8744231540", // For testing only
+      tokenId: 14,
+      cipherRef: "giftcode_1779329986485.bin",
+      aesKeyHex: "0xdf5773b26ebaa91d05056cb6fe08d921", // For testing only
+      ciphertextHex:
+        "52e39afc636a15b788e374e10d7254da529748b003c0cf1a864e7d3d32238b8bbfa49e23526cf4b7cca0548ac38e8a19aeb6aaaf4b647a3774179320237bcd6c81d2a53cd9dcef41", // Encrypted giftcode as hex
     },
   },
   vault: {
